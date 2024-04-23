f0f3fc4166e24f84eab1cad5fd2c811d1172edbf
David-Salihu <davidsalihu19@gmail.com>
2024-04-23 11:36:36 +0100

    Implement props and mapping for notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,14 @@ import React from "react"
 import Header from "./Header"
 import Footer from "./Footer";
 import Note from "./Note"
-
+import notes from "../notes"
 
 function App() {
     return (
         <div>
             <Header/>
-            <Note/>
-            <Footer />
+            {notes.map((note) => <Note title={note.title} content={note.content}/>)}
+        <Footer />
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/"
+import App from "./components/App"
 
 ReactDOM.render( 
   <App />
